05375b0d4dbf3c8910265e0f6123c945ee6b3e35
Lou1s-Lau <louisliulys@gmail.com>
2025-04-29 05:04:14 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SimpleClick",
+    version="1.0",
+    packages=find_packages(where="SimpleClick-1.0"),
+    package_dir={"": "SimpleClick-1.0"},
+)
